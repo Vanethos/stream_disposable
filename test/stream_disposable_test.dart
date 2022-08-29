35e64f207e20615a09eb6d1ee1af4943eb008c01
Vanethos <solid.goncalo@gmail.com>
2022-08-29 12:14:22 +0100

    Removes import

--- test/stream_disposable_test.dart
+++ test/stream_disposable_test.dart
@@ -1,7 +1,6 @@
 import 'dart:async';
 
 import 'package:flutter_test/flutter_test.dart';
-import 'package:matcher/matcher.dart';
 import 'package:stream_disposable/stream_disposable.dart';
 
 void main() {
